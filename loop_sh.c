e75bb351473dd52419c271e5c7ea6e99393a5097
Ian Mike <iammikeian@gmail.com>
2023-08-23 15:43:13 +0300

    Create comment checker on user input

--- loop_sh.c
+++ loop_sh.c
@@ -17,6 +17,7 @@ void loop_sh(arg_list *shell_data)
 		input = read_line(&eof_input);
 		if (eof_input != -1)
 		{
+			input = comment_del(input);
 			if (input == NULL)
 				continue;
 
@@ -37,4 +38,36 @@ void loop_sh(arg_list *shell_data)
 			free(input);
 		}
 	}
+}
+
+/**
+ * comment_del - delete comments from input
+ * @in: input str
+ * Return: input with no comments
+ */
+char *comment_del(char *in)
+{
+	int i, up_to;
+
+	up_to = 0;
+	for (i = 0; in[i]; i++)
+	{
+		if (in[i] == '#')
+		{
+			if (i == 0)
+			{
+				free(in);
+				return (NULL);
+			}
+
+			if (in[i - 1] == ' ' || in[i - 1] == '\t' || in[i - 1] == ';')
+				up_to = i;
+		}
+	}
+	if (up_to != 0)
+	{
+		in = _realloc(in, i, up_to + 1);
+		in[up_to] = '\0';
+	}
+	return (in);
 }
